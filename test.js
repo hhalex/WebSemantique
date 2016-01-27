0ff0ea13f3722f5b342ac1bdc2f01b20fbb7b8d2
alexandre <alexandre.careil@supelec.fr>
2016-01-27 16:44:53 +0100

    Form improvements, project name: RiiiiiiseMusic

--- test.js
+++ test.js
@@ -5,20 +5,20 @@ vie.loadSchema("http://schema.rdfs.org/all.json",
     baseNS : "http://schema.org",
     success: function () {
         alert('success');
-        jQuery('#schemaOrg .resultsholder').append('<div class="msg">Successfully loaded the schema ontology!</di>');
-        jQuery('#schemaOrg .resultsholder').append('<div class="msg">We now have ' + this.types.list().length + ' classes loaded!</di>');
+        jQuery('#schemaOrg .resultsholder').append('<div class="bg-success">Successfully loaded the schema ontology!</di>');
+        jQuery('#schemaOrg .resultsholder').append('<div class="bg-success">We now have ' + this.types.list().length + ' classes loaded!</di>');
         var Place = this.types.get("Place");
         var City = this.types.get("City");
         var Person = this.types.get("Person");
-        jQuery("#results").append('<div class="msg">BTW (1): A schema:City is <b>' + 
+        jQuery("#results").append('<div class="bg-info">BTW (1): A schema:City is <b>' + 
                 ((City.isof(Place))? ' ' : 'not ') + 
                 '</b>of type schema:Place, but <b>' + 
                 ((City.isof(Person))? ' ' : 'not ') + 
                 '</b>of schema:Person!</div>');
-        jQuery("#results").append('<div class="msg">BTW (2): A schema:City has <b>' + City.attributes.list().length + ' attributes, including all inherited!</div>');
+        jQuery("#results").append('<div class="bg-info">BTW (2): A schema:City has <b>' + City.attributes.list().length + ' attributes, including all inherited!</div>');
     },
     error: function () {
         alert('RiiiiiiiiseClipse');
-        jQuery("#results").append('<div class="msg">Something went wrong with loading the ontology!</di>');
+        jQuery("#results").append('<div class="bg-danger">Something went wrong with loading the ontology!</di>');
     }
 });
