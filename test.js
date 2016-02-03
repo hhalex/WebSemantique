424b99ba40af3cf869124afb6777d7048cf7ff59
Alexandre Careil <alexandre@MacBook-Pro-de-Alexandre.local>
2016-02-03 15:37:02 +0100

    Changement de design

--- test.js
+++ test.js
@@ -6,26 +6,48 @@ $(document).ready(function () {
         .prefix('prop', 'http://dbpedia.org/property/')
         .prefix('res', 'http://dbpedia.org/resource/')
         .prefix("rdfs","http://www.w3.org/2000/01/rdf-schema#")
-        .select(["?genre", "?label", "?year", "?album", "?album_name"])
+        .select(["?genre", "?label", "?year"])
+    //    .select(["?genre", "?label", "?year", "?album", "?album_name"])
         .where("?genre","a","onto:MusicGenre")
         .where("?genre","rdfs:label","?label")
         .where("?genre","prop:popularity","?year")
-        .where("?album","a","onto:Album")
-        .where("?album","prop:genre","?genre")
-        .where("?album","rdfs:label","?album_name")
-        .filter("xsd:integer(?year) > 2000 and xsd:integer(?year) < 2010")
-        .filter("lang(?label) = 'en' or lang(?label) = 'fr'")
-          .distinct("?album")
-          .distinct("?label")
+    //    .where("?album","a","onto:Album")
+    //    .where("?album","prop:genre","?genre")
+    //    .where("?album","rdfs:label","?album_name")
+        .filter("xsd:integer(?year) >= 1980")
+        .filter("lang(?label) = 'fr'")
+      //    .distinct("?album")
+          .distinct("?genre")
         //.orderby("xsd:integer(?nbA)")
         .limit(100)
         //.select(["?genre"])
        // .where('?genre', 'a', 'onto:MusicGenre')
         .execute(function(res) {
-            alert("aa");
+          $('#wordcloud2').empty();
             for (el in res)
-                $('#list')
-                .append('<li><a href="' + res[el].album.uri + '">'+res[el].album_name+'</a> (genre <a href="' + res[el].genre.uri + '">'+res[el].label+'</a>) : '+res[el].year+'</li>');
+            {
+                if ('genre' in res[el])
+                {
+                  var dataweight = parseInt((parseInt(res[el].year)-1980)*(1+Math.random()));
+                  $('#wordcloud2')
+                  .append('<span data-weight="'+dataweight+'"><a href="' + res[el].genre.uri + '">'+res[el].label.replace('&', 'n')+'</a></span>');
+                }
+            }
+
+              $("#wordcloud2").awesomeCloud({
+                "size" : {
+                  "grid" : 9,
+                  "factor" : 1
+                },
+                "options" : {
+                  "color" : "random-dark",
+                  "rotationRatio" : 0.35
+                },
+                "font" : "'Times New Roman', Times, serif",
+                "shape" : "circle"
+              });
+
+
             return false;
         });
 });
