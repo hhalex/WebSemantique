b00ae5b2c91b19acf0594fd5f847a68dfe8d64f2
alexandre <alexandre.careil@supelec.fr>
2016-02-03 01:22:27 +0100

    Requête sparql fonctionnelle pour les genres musicaux

--- fetch_all.sh
+++ fetch_all.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
+rm -rf data/* $2> /dev/null
 mkdir lib/ &2> /dev/null
-mkdir data/ &2> /dev/null
+#mkdir data/ &2> /dev/null
 wget "http://viejs.org/js/vie-2.1.0.js" -O "lib/vie.js"
-wget "http://moustaki.org/resources/jamendo-rdf.tar.gz" -O "data/jamendo-rdf.tar.gz"
+#wget "http://moustaki.org/resources/jamendo-rdf.tar.gz" -O "data/jamendo-rdf.tar.gz"
 wget "http://code.jquery.com/jquery-1.12.0.min.js" -O "lib/jquery.js"
 wget "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rdfquery/jquery.rdfquery.core.min-1.0.js" -O "lib/jquery-rdf.js"
 wget "https://github.com/twbs/bootstrap/releases/download/v3.3.6/bootstrap-3.3.6-dist.zip" -O "lib/bootstrap.zip"
-wget "http://backbonejs.org/backbone-min.js" -O "lib/backbone.js"
-wget "http://underscorejs.org/underscore-min.js" -O "lib/underscore.js"
+#wget "http://backbonejs.org/backbone-min.js" -O "lib/backbone.js"
+#wget "http://underscorejs.org/underscore-min.js" -O "lib/underscore.js"
 
-$(cd ./lib && unzip *.zip && rm -f *.zip && cd ..)
-$(cd ./data && tar xzf *.tar.gz && rm -f *.tar.gz && cd ..)
+cd lib/
+unzip -a *.zip
+rm -f *.zip
+cd ..
+
+#$(cd ./lib && unzip *.zip && rm -f *.zip && cd ..)
+#$(cd ./data && tar xzf *.tar.gz && rm -f *.tar.gz && cd ..)
 
 echo "Installation finie"
 
--- test.js
+++ test.js
@@ -1,24 +1,20 @@
-var vie = new VIE();
-var results= jQuery("#results");
-vie.loadSchema("http://schema.rdfs.org/all.json", 
-{
-    baseNS : "http://schema.org",
-    success: function () {
-        alert('success');
-        jQuery('#schemaOrg .resultsholder').append('<div class="bg-success">Successfully loaded the schema ontology!</di>');
-        jQuery('#schemaOrg .resultsholder').append('<div class="bg-success">We now have ' + this.types.list().length + ' classes loaded!</di>');
-        var Place = this.types.get("Place");
-        var City = this.types.get("City");
-        var Person = this.types.get("Person");
-        jQuery("#results").append('<div class="bg-info">BTW (1): A schema:City is <b>' + 
-                ((City.isof(Place))? ' ' : 'not ') + 
-                '</b>of type schema:Place, but <b>' + 
-                ((City.isof(Person))? ' ' : 'not ') + 
-                '</b>of schema:Person!</div>');
-        jQuery("#results").append('<div class="bg-info">BTW (2): A schema:City has <b>' + City.attributes.list().length + ' attributes, including all inherited!</div>');
-    },
-    error: function () {
-        alert('RiiiiiiiiseClipse');
-        jQuery("#results").append('<div class="bg-danger">Something went wrong with loading the ontology!</di>');
-    }
+$(document).ready(function () {
+    $("#list").empty();
+    $("#list").append('<li><b>Liste des genres</b></li>');
+    $.sparql("http://dbpedia.org/sparql")
+        .prefix('onto', 'http://dbpedia.org/ontology/')
+        .prefix('prop', 'http://dbpedia.org/property/')
+        .prefix("rdfs","http://www.w3.org/2000/01/rdf-schema#")
+        .select(["?genre"])
+        .where("?genre","a","onto:MusicGenre")
+        .limit(100)
+        //.select(["?genre"])
+       // .where('?genre', 'a', 'onto:MusicGenre')
+        .execute(function(res) {
+            alert("aa");
+            for (el in res)
+                $('#list')
+                .append('<li>' + res[el].genre.uri + '</li>');
+            return false;
+        });
 });
