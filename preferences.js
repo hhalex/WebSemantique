cb39d6a84d30e2db3591ecb1d1af5e4deae0cbd4
araelath <jeanb.rocher@gmail.com>
2016-02-20 16:50:00 +0100

    animation javascript pour la page d'affichage des chansons aimées

---
+++ preferences.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  var cover = $(".cover");
+
+  cover.hover(function(){
+    $(this).parent().css("box-shadow", "10px 10px 5px #000000");
+    $(this).css("width", "330px");
+    $(this).css("height", "330px");
+
+  });
+  cover.mouseleave(function(){
+    $(this).parent().css("box-shadow", "0px 0px 0px #000000");
+    $(this).css("width", "300px");
+    $(this).css("height", "300px");
+
+  });
+
+
+
+});
