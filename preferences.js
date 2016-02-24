ca95a77eb062f2a498e5b3942f0593ea0ccee131
araelath <jeanb.rocher@gmail.com>
2016-02-24 14:46:40 +0100

    code plus propre

--- preferences.js
+++ preferences.js
@@ -1,34 +1,22 @@
 function hoverCover(){
 
-  $(this).parent().css("box-shadow", "10px 10px 5px #000000");
-  $(this).css("width", "220px");
-  $(this).css("height", "220px");
+  $(this).addClass("coverHovered");
 
 }
 function leaveCover(){
-  $(this).parent().css("box-shadow", "0px 0px 0px #000000");
-  $(this).css("width", "200px");
-  $(this).css("height", "200px");
+  $(this).removeClass("coverHovered");
 
 }
 function open() {
 
-  $(".lightbox").on('click', '.close', close);
+
   $(".lightbox").off('click', '.album');
   $(".lightbox").off('mouseover', '.cover');
   $(".lightbox").off('mouseout', '.cover');
-    $(".lightbox").on('click', '.close', close);
+  $(".lightbox").on('click', '.close', close);
 
-  $(this).css({
-
-    position: 'fixed',
-    display: 'block',
-    color: 'white',
-    boxShadow: "0px 0px 0px #000000",
-    zIndex:'9999'
-  });
-
-
+  $(this).addClass('albumExtend');
+  $(this).find(".cover").removeClass("coverHovered");
   $(this).find(".cover").css('display', 'inline-block');
   $(".album").not(this).find('.cover').css(
 
@@ -38,12 +26,9 @@ function open() {
     }
   )
 
-  $(this).css('background-color', 'black');
 
 
   $(this).find(".cover").animate({
-
-
 
     width :'220px',
     height: '220px',
@@ -57,9 +42,7 @@ function open() {
 
 
     paddingRight: '10px',
-
     width: '440px',
-
 
     left :'50%',
     top: '50%',
@@ -69,9 +52,8 @@ function open() {
 
   },function(){
 
-    $(this).find(".secret").not('h1').not('h2').css("display", "inline-block");
-    $(this).find('h2').css("display" ,"block");
-    $(this).find('h3').css("display", "block");
+    $(this).find(".secret").addClass('secretExtended');
+
 
 
   });
@@ -84,11 +66,13 @@ function close () {
   $(".secret").removeAttr("style");
   $(".cover").removeAttr("style");
   $(".album").removeAttr("style");
+  $(".album").removeClass('albumExtend');
+  $(".secret").removeClass('secretExtended');
 
   $(".lightbox").on('click', '.album', open);
   $(".lightbox").on('mouseover', '.cover', hoverCover);
   $(".lightbox").on('mouseout', '.cover', leaveCover);
-  $(".lightbox").off('click', '.close', close);
+  $(".lightbox").off('click', '.close');
 
 
 
